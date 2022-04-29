d42eb986b9dbebd29d0a6625467882ec13421f8d
zeming666 <zr109@uowmail.edu.au>
2022-04-29 13:58:15 +1000

    1

---
+++ javatest/src/main.java
@@ -0,0 +1,12 @@
+public class main {
+    public static void main(String[] args) {
+
+    }
+    public static int cul(){
+        int x = 3;
+        int y = 4;
+        int res;
+        res = x+y;
+        return res;
+    }
+}
